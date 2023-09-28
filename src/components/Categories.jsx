a5a116f7f512ee00fac0abc57e639d51b0cb14b9
Daniel Blanco <db@d-blanco.com>
2023-09-28 18:57:39 -0400

    button and checkbox - WIP

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,15 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 import './styles.css';
 
-const Icon = () => {
-  return (
-    <svg height="20" width="20" viewBox="0 0 20 20">
-      <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
-    </svg>
-  );
-};
+const Icon = () => (<svg height="20" width="20" viewBox="0 0 20 20">^</svg>);
 
 const DropdownCategories = ({ placeholder, options }) => {
+  const [mark, setMark] = useState(false);
+  const [read, setRead] = useState('UNREAD');
+  const [active, setActive] = useState('INACTIVE');
   const [showMenu, setShowMenu] = useState(false);
   const [selectCat, setSelectCat] = useState(null);
 
@@ -23,22 +20,30 @@ const DropdownCategories = ({ placeholder, options }) => {
     };
   });
 
-  // const selectCategory = () => {
-  //   return (
-  //     <div className="dropdown--menu">
-  //       {options.map((option, i) => (
-  //         <div
-  //           key={i}
-  //           className="dropdown--item"
-  //           style={{
-  //             color: `${option.color}`,
-  //             fontWeight: 'bold',
-  //           }}>
-  //           {option.name}
-  //         </div>
-  //       ))}
-  //     </div>)
-  // };
+  const selectCategory = () => {
+    return (
+      <div className="dropdown--menu">
+
+        {options.map(option => (
+          <div
+            key={option.name}
+            className="dropdown--item categories"
+            style={{
+              background: `${option.color}`,
+              color: '#fff',
+              fontWeight: 'bold',
+            }}>
+            {option.name}
+          </div>
+        ))}
+      </div>
+    )
+  };
+
+  const checkbox = () => setMark(!mark);
+  const alreadyRead = () => setRead('READ');
+
+  const activateColors = () => setActive('ACTIVE');
 
   // const handleInputClick = (e) => {
   //   e.stopPropagation();
@@ -49,33 +54,38 @@ const DropdownCategories = ({ placeholder, options }) => {
     if (selectCat) return selectCat.name;
     return placeholder;
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <>
-      <h5 className="titles">CATEGORIES</h5>
-      <div className="dropdown--container">
-        <div className="dropdown--input">
-          <div className="dropdown--selected--value">{getDisplay()}</div>
-          <div className="dropdown--menu">
-            {options.map(option => (
-              <div
-                key={option.name}
-                className="dropdown--item categories"
-                style={{
-                  background: `${option.color}`,
-                  color: '#fff',
-                  fontWeight: 'bold',
-                }}>
-                {option.name}
-              </div>
-            ))}
+      <div>
+        <button type="button">{mark ? 'INACTIVE' : 'ACTIVE'}</button>
+      </div>
+      <div className="line--divider" />
+      <div>
+        {/* <form onSubmit={handleSubmit}> */}
+        <div>
+          <div>
+            <input type="checkbox" onChange={checkbox} />
+            <div>PORQUE NO VEO EL CHECKBOX</div>
           </div>
-          <div className="dropdown--tools">
-            <div className="dropdown--tool">
-              <Icon />
+        </div>
+
+        <div className="line--divider" />
+        <h5 className="titles">CATEGORIES</h5>
+        <div className="line--divider" />
+        <div className="dropdown--container">
+          <div className="dropdown--input">
+            <div className="dropdown--selected--value">{getDisplay()}</div>
+            {/* {selectCategory()} */}
+            <div className="dropdown--tools">
+              <div className="dropdown--tool"><Icon /></div>
             </div>
           </div>
         </div>
+        {/* </form> */}
       </div>
     </>
   );
