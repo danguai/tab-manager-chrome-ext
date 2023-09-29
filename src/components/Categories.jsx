dce9a4e8e0dab36d4fbe034528a56d1715b5fc49
Daniel Blanco <db@d-blanco.com>
2023-09-29 11:31:07 -0400

    dropdown is dropingdown :D - WIP

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,17 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 import './styles.css';
 
-const Icon = () => (<svg height="20" width="20" viewBox="0 0 20 20">^</svg>);
+const Icon = () => (
+  <svg height="20" width="20" viewBox="0 0 20 20">
+    <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+  </svg>
+);
 
 const DropdownCategories = ({ placeholder, options }) => {
   const [mark, setMark] = useState(false);
-  const [read, setRead] = useState('UNREAD');
-  const [active, setActive] = useState('INACTIVE');
-  const [showMenu, setShowMenu] = useState(false);
+
+  const [activate, setActivate] = useState(false);
+  const [activateButtonName, setActivateButtonName] = useState('INACTIVE');
+
+  const [dropdownMenu, setDropdownMenu] = useState(false);
   const [selectCat, setSelectCat] = useState(null);
 
   useEffect(() => {
-    const handler = () => setShowMenu(false);
+    const handler = () => setDropdownMenu(!dropdownMenu);
 
     window.addEventListener('click', handler);
     return () => {
@@ -20,76 +26,84 @@ const DropdownCategories = ({ placeholder, options }) => {
     };
   });
 
-  const selectCategory = () => {
-    return (
-      <div className="dropdown--menu">
+  const isSelected = (option) => {
+    if (!selectCat) return false;
+    return selectCat.name === option.name;
+  };
 
-        {options.map(option => (
-          <div
-            key={option.name}
-            className="dropdown--item categories"
-            style={{
-              background: `${option.color}`,
-              color: '#fff',
-              fontWeight: 'bold',
-            }}>
-            {option.name}
-          </div>
-        ))}
-      </div>
-    )
+  const selectCategory = () => (
+    <div className="dropdown--menu">
+      {options.map(option => (
+        <div
+          onClick={(option => setSelectCat(option))}
+          key={option.name}
+          className={`dropdown--item categories`}
+          style={{
+            background: `${option.color}`,
+            color: '#fff',
+            fontWeight: 'bold',
+          }}>
+          {option.name}
+        </div>
+      ))}
+    </div>
+  );
+
+  const activateColors = () => {
+    setActivate(!activate);
+    setActivateButtonName('ACTIVE');
   };
 
   const checkbox = () => setMark(!mark);
-  const alreadyRead = () => setRead('READ');
 
-  const activateColors = () => setActive('ACTIVE');
-
-  // const handleInputClick = (e) => {
-  //   e.stopPropagation();
-  //   setShowMenu(!showMenu);
-  // };
+  const showMenu = (e) => {
+    e.stopPropagation();
+    setDropdownMenu(!dropdownMenu);
+  };
 
   const getDisplay = () => {
     if (selectCat) return selectCat.name;
     return placeholder;
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
 
   return (
     <>
       <div>
-        <button type="button">{mark ? 'INACTIVE' : 'ACTIVE'}</button>
+        <button
+          type="button"
+        // onClick={activateColors()}
+        >
+          {activateButtonName}
+        </button>
       </div>
-      <div className="line--divider" />
-      <div>
-        {/* <form onSubmit={handleSubmit}> */}
-        <div>
-          <div>
-            <input type="checkbox" onChange={checkbox} />
-            <div>PORQUE NO VEO EL CHECKBOX</div>
-          </div>
-        </div>
 
-        <div className="line--divider" />
-        <h5 className="titles">CATEGORIES</h5>
-        <div className="line--divider" />
-        <div className="dropdown--container">
-          <div className="dropdown--input">
+      <div className="line--divider" />
+
+      <div>
+        WHERE'S THE FUCKING CHECKBOX
+        <div>
+          <input type="checkbox" onChange={checkbox} />
+        </div>
+      </div>
+
+      <div className="line--divider" />
+
+      <h5 className="titles">CATEGORIES</h5>
+      <div className="line--divider" />
+
+      <div className="dropdown--container">
+        {dropdownMenu && (
+          <div onClick={showMenu} className="dropdown--input">
             <div className="dropdown--selected--value">{getDisplay()}</div>
-            {/* {selectCategory()} */}
+            {selectCategory()}
             <div className="dropdown--tools">
               <div className="dropdown--tool"><Icon /></div>
             </div>
           </div>
-        </div>
-        {/* </form> */}
+        )}
       </div>
     </>
   );
 };
-
 
 export default DropdownCategories;
